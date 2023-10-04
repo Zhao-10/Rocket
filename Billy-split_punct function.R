ea44c5c06cfb55cd8e1a75613184351fe290c351
Billy Debruyne <KanoDaisuki25@users.noreply.github.com>
2023-10-04 21:51:23 +0100

    202310042152Updated

--- Billy-split_punct function.R
+++ Billy-split_punct function.R
@@ -1,4 +1,4 @@
-# Billy: split_punct function
+# Billy Q1-7
 
 ## Pre-possessing
 a = scan("/Users/billy/Downloads/Edin/Edin-study/Sem1/Extended\ Statistical\ Programming/Assignments/Practical1/4300-0.txt", what="character", skip=73,nlines=32858-73)
@@ -37,3 +37,40 @@ split_punct = function(billy){
   
   return(vec)
 }
+
+
+
+
+## Question 6-9
+
+### 6(a)
+an1 = tolower(an)
+an2 = unique(an1)
+# 6(b)
+an3 = match(an1, an2)
+### 6(c)
+tabulate(an3)
+
+### 6(d)
+sort(tabulate(an3), decreasing = TRUE)[1000] ### result=27
+b_len = table(tabulate(an3)>=27)["TRUE"]
+
+### 6(e)
+an2_index = c(1:length(an2))
+df = data.frame(an2_index, tabulate(an3)
+)
+newdf = df[order(-tabulate(an3)),]
+newdf = newdf[1: b_len,]
+b = an2[newdf$an2_index]
+
+
+### 7(a)
+b1 = match(an1,b)
+### 7(b+c)
+T = cbind(b1[1:(length(b1)-2)], b1[2:(length(b1)-1)], b1[3:length(b1)])
+T = T[rowSums(is.na(T)) == 0, ]
+### 7(d)
+P = cbind(b1[1:(length(b1)-1)], b1[2:(length(b1))])
+P = P[rowSums(is.na(P)) == 0, ]
+
+### 8
