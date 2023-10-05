59f141c798d1c0cc3e8c69e6c268d1bbc376e893
Billy Debruyne <145297390+KanoDaisuki25@users.noreply.github.com>
2023-10-05 13:58:06 +0100

    Update B202310051400

--- Billy-split_punct function.R
+++ Billy-split_punct function.R
@@ -1,14 +1,23 @@
-# Billy Q1-7
+# Billy Practical1
 
 ## Pre-possessing
-a = scan("/Users/billy/Downloads/Edin/Edin-study/Sem1/Extended\ Statistical\ Programming/Assignments/Practical1/4300-0.txt", what="character", skip=73,nlines=32858-73)
-a = gsub(")_","",gsub("_(","",a,fixed=TRUE),fixed=TRUE) ## remove "_(" and ")_"
+
+## Use "scan" function to split the txt according to blank space
+## a = scan(file directory, what="character", skip=73,nlines=32858-73) 
+
+## replace _( and )_ with nothing
+a = gsub(")_","",gsub("_(","",a,fixed=TRUE),fixed=TRUE) 
+## replace _ appearing at the end of the entry with nothing
 a = gsub("\\_$","",a)
+
+## replace . appearing no less than 4 times at the end of the entry with nothing
 a = gsub("\\.{4,}$","",a)
 
+## replace entries containing only punct with nothing
 a = gsub("^[[:punct:]]+$","",a)
+## replace puncts starting the entries with nothing
 a = gsub("^[[:punct:]]+","",a)
-
+## eliminate ""
 a = a[a != ""]
 
 
@@ -74,3 +83,43 @@ P = cbind(b1[1:(length(b1)-1)], b1[2:(length(b1))])
 P = P[rowSums(is.na(P)) == 0, ]
 
 ### 8
+
+
+b_tf = sum(newdf$tabulate.an3.)
+b_sf = newdf$tabulate.an3./b_tf
+
+model_sim = function(b_p){
+  sim = rep(NA, 52)
+  sim[1:2] = sample(1:1007,2)
+  for (i in 3:52){
+    Mat1 = T[(T[,1] == sim[i-2]) & (T[,2] == sim[i-1]),]
+    Mat2 = P[(P[,2] == sim[i-1]),]
+    if (isTRUE(nrow(Mat1)) != 0) {
+      bdf1 = as.data.frame(table(Mat1[,3]))
+      accumulate_b_p1 = b_p[bdf1$Var1]*bdf1$Freq
+      conditional_b_p1 = accumulate_b_p1/sum(accumulate_b_p1)
+      sim[i] = sample(bdf1$Var1, size = 1, prob = conditional_b_p1)
+    }
+    
+    else if (isTRUE(nrow(Mat2)) != 0){
+      bdf2 = as.data.frame(table(Mat2[,2]))
+      accumulate_b_p2 = b_p[bdf2$Var1]*bdf2$Freq
+      conditional_b_p2 = accumulate_b_p2/sum(accumulate_b_p2)
+      sim[i] = sample(bdf2$Var1, size = 1, prob = conditional_b_p2)
+    }
+    
+    else {sim[i] = sample(1:1007, size = 1, prob = b_p)}
+  }
+  sim = b[sim[3:52]]
+  return(sim)
+}
+
+#### Testing
+b_p = ruinif(1007); b_p = b_p/sum(b_p)
+cat(model_sim(b_p)," ")
+
+
+### 9
+sample(b, size = 50, replace = TRUE ,prob = b_p)
+
+### 10 
